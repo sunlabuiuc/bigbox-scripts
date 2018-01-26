b6a6e6f2cc61bfa66da4c25ac291c404358b1474
Yu Jing <yujing5b5d@gmail.com>
2018-01-25 21:38:19 -0500

    patch will only write to zeppelin-env

--- install-prerequisites-for-py35-in-zeppelin.sh
+++ install-prerequisites-for-py35-in-zeppelin.sh
@@ -4,7 +4,8 @@ su zeppelin -c 'conda create -n conda_env_py35 -y'
 
 # LINE="source activate conda_env_py35"
 LINE='export PATH=/home/zeppelin/.conda/envs/conda_env_py35/bin:$PATH'
-TARGET_FILE=/etc/profile.d/bigbox.sh
+# TARGET_FILE=/etc/profile.d/bigbox.sh
+TARGET_FILE=/usr/local/zeppelin/conf/zeppelin-env.sh
 
 if grep -Fxq "$LINE" $TARGET_FILE; then
   # Found.. just ignore it
