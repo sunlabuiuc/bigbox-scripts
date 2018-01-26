b8bbb1efc8963b0726daba80d57f2f975fe07ce6
Yu Jing <yujing5b5d@gmail.com>
2018-01-26 00:04:30 -0500

    better start/stop jupyter

--- start-jupyter.sh
+++ start-jupyter.sh
@@ -11,12 +11,14 @@ do_serve() {
   nohup jupyter notebook --port=8888 --no-browser --ip=0.0.0.0 --allow-root --NotebookApp.token='' \
       > $LOG_FILE 2>&1  & echo $! > $PID_FILE
   echo_success
-  echo "Jupyter Is Started, Log File: $LOG_FILE Pid File: $PID_FILE"
+  echo "Jupyter Is Started"
+  echo "You can find log file: $LOG_FILE"
 }
 
-if status -p $PID_FILE jupyter ; then
-  echo "Jupyter Is Already Running, Log File: $LOG_FILE Pid File: $PID_FILE"
+if status -p $PID_FILE jupyter >> /dev/null ; then
   echo_failure
+  status -p $PID_FILE jupyter
+  echo "You can find log file: $LOG_FILE"
 else
 
   if hash jupyter  2>/dev/null ; then
--- stop-jupyter.sh
+++ stop-jupyter.sh
@@ -7,14 +7,24 @@ PID_FILE=/var/run/jupyter.pid
 LOG_FILE=/var/log/jupyter.log
 
 if [ -f $PID_FILE ]; then
-  JPID=$(cat $PID_FILE )
-  echo "Stop Jupyter Service..."
-  kill -9 $JPID
-  rm -rf $PID_FILE
-  echo "Log File: $LOG_FILE"
-  echo "Latest Logs: "
-  tail -n 10 $LOG_FILE
+  if status -p $PID_FILE jupyter >> /dev/null ; then
+    # is running
+    JPID=$(cat $PID_FILE )
+    echo_success
+    echo "Stop Jupyter Service..."
+    kill -9 $JPID
+    rm -f $PID_FILE
+    echo "Log File: $LOG_FILE"
+    echo "Latest Logs: "
+    [ -f $LOG_FILE ] && tail -n 10 $LOG_FILE
+  else
+    echo_success
+    status -p $PID_FILE jupyter
+    rm -f $PID_FILE
+  fi
+
 else
-  echo "Stop Jupyter Failed: Pid File Not Found"
+  echo_success
+  status -p $PID_FILE jupyter
 fi
 
