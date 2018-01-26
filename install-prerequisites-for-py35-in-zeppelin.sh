8eb72583ec6e01230f1e4ed7a51ee3a62f1f865b
Yu Jing <yujing5b5d@gmail.com>
2018-01-25 20:22:57 -0500

    add prerequisites for py35 in zeppelin

---
+++ install-prerequisites-for-py35-in-zeppelin.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+su zeppelin -c 'conda create -n conda_env_py35 -y'
+
+# LINE="source activate conda_env_py35"
+LINE='export PATH=/home/zeppelin/.conda/envs/conda_env_py35/bin:$PATH'
+TARGET_FILE=/etc/profile.d/bigbox.sh
+
+if grep -Fxq "$LINE" $TARGET_FILE; then
+  # Found.. just ignore it
+  echo "Already Applied"
+else
+  echo "$LINE" >> $TARGET_FILE
+fi
+
+su zeppelin -c 'source activate conda_env_py35 ; conda install python=3.5 py4j matplotlib pyspark -y; which python'
+
